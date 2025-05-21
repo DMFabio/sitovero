d336a0935d5918bc3816221f938bbb80335b0f18
DMFabio <fabio.demicco@gmail.com>
2025-05-21 20:30:15 +0200

    aggiunta navbar

---
+++ src/lib/components/partials/navbar-link.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	type Attributo = {
+		url: string;
+		testo: string;
+	};
+
+	let a: Attributo = $props();
+</script>
+
+<a
+	href={a.url}
+	class="bg-primary text-primary-foreground hover:bg-secondary hover:text-primary-foreground"
+	>{a.testo}</a
+>
---
+++ src/lib/components/partials/navbar.svelte
@@ -0,0 +1,13 @@
+<script>
+	import NavbarLink from './navbar-link.svelte';
+</script>
+
+<nav class="bg-primary text-primary-foreground flex justify-between">
+	<NavbarLink url="/" testo="Home" />
+
+	<div>
+		<NavbarLink url="/casabachi" testo="Casa Bachi" />
+		<NavbarLink url="/contatti" testo="Contatti" />
+		<NavbarLink url="/progetti" testo="Progetti" />
+	</div>
+</nav>
