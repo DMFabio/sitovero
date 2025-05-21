fa9c9ffbde077b62cfae7b8a1b88128a25dc85fa
DMFabio <fabio.demicco@gmail.com>
2025-05-21 19:12:23 +0200

    aggiunta pagine e parte di contenuti

---
+++ src/routes/casabachi/+page.svelte
@@ -0,0 +1,13 @@
+<h1>CASA BACHI</h1>
+<br />
+<h1>COSA È CASA BACHI?</h1>
+<br />
+<p>
+	Casa Bachi è lo spazio in cui convergono gli interessi artistici e culturali dell’associazione
+	Bachi da Setola, un aggregatore sociale e organizzatore di eventi musicali, teatrali e di progetti
+	di scambio all’interno della comunità europea. La casa è un vivo spazio di dialogo con la
+	comunità, offre accoglienza e ristoro ma anche possibilità di crescita con workshop, laboratori,
+	residenze, concerti, presentazioni di libri, occasioni di dibattito e di svago. Le attività sono
+	rivolte all’intera comunità locale, da bambinə a ragazzə ad adultə, senza tralasciare la
+	componente del turismo stagionale.
+</p>
---
+++ src/routes/contatti/+page.svelte
@@ -0,0 +1,14 @@
+<h1>CONTATTI</h1>
+<h2>
+	Il nostro è uno spazio aperto ad idee, proposte, progetti, condivisione. Se pensi di poter dare un
+	contributo valido a Casa Bachi o vuoi chiedere maggiori informazioni riguardo le nostre
+	iniziative, compila il form sottostante o contattaci tramite i riferimenti indicati in questa
+	pagina.
+</h2>
+
+<br />
+
+<p>
+	I nostri riferimenti: <br /> EMAIL info@casabachi.it <br /> DOVE SIAMO Via Luigi Pirandello, 12 70044
+	Polignano a Mare / Bari
+</p>
